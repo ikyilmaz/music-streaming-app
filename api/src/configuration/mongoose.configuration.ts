bc4b22f7024c27fee6b7f96547f041eb18eec9b2
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-12-08 22:45:01 +0300

    bazı geliştirmeler

--- api/src/configuration/mongoose.configuration.ts
+++ api/src/configuration/mongoose.configuration.ts
@@ -2,19 +2,26 @@ import { ConfigService } from '@nestjs/config';
 import { MongooseModuleOptions } from '@nestjs/mongoose';
 
 export const mongooseOptions = (
-	configService: ConfigService,
+  configService: ConfigService,
 ): Promise<MongooseModuleOptions> | MongooseModuleOptions => {
-	let uri = configService.get<string>('DATABASE_URL');
+  let uri = configService.get<string>('DATABASE_URL');
 
-	uri = uri.replace('<username>', configService.get<string>('DATABASE_USERNAME'));
-	uri = uri.replace('<password>', configService.get<string>('DATABASE_PASSWORD'));
-	uri = uri.replace('<dbname>', configService.get<string>('DATABASE_NAME'));
-
-	return {
-		uri,
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true,
-	};
+  uri = uri.replace(
+    '<username>',
+    configService.get<string>('DATABASE_USERNAME'),
+  );
+  uri = uri.replace(
+    '<password>',
+    configService.get<string>('DATABASE_PASSWORD'),
+  );
+  uri = uri.replace('<dbname>', configService.get<string>('DATABASE_NAME'));
+ 
+  return {
+    uri:
+      process.env.NODE_ENV === 'production' ? uri : 'mongodb://localhost:27017',
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  };
 };
